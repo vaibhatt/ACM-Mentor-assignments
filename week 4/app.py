0e32a9a20b93e0d5f5ed3ee26a7ac26f9e6e3764
Shreyansh Singh <45299337+iamshr3y@users.noreply.github.com>
2020-08-09 19:11:39 +0530

    build web app backend

--- week 4/app.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-from recommendation import recommend
-import flask
-
-app = flask.Flask(__name__,template_folder='templates')
-
-
-
---
+++ week 4/webapp/__int__.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from recommendation import recommend
+import flask
+
+app = flask.Flask(__name__,template_folder='templates')
+
+@app.route('/')
+def root():
+	return render_template("index.html")
+
+@app.route("/", methods = ["GET", "POST"])
+def get_movie():
+	if request.method == "POST":
+		default_name = "0"
+
+		movie_name = request.form.get("movie", default_name)
+
+		num_recommends = 10
+		out_df = recommendation.recommend(movie_name, num_recommends)
+
+		return render_template("out.html", name = "Recommended Movies", data = x.to_html())
